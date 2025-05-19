bb04afd42fe463f371880a3e454a2d5cb2ed05fe
chrisferrer89 <chrisferrer89@gmail.com>
2025-05-19 03:28:25 +0000

    Update excuse generator with final version

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var who = ['a one-eyed Cuban', 'Your mom', 'Don Francisco', 'Scarlett Johansson'];
+  var action = ['squeezed', 'destroyed', 'milked', 'spit on'];
+  var what = ['my sweet cat', 'my toilet', 'my nose', 'my knee'];
+  var when = ['in my bathroom', 'while I was doing this project', 'during my morning affirmations', 'as I baked warm bagels'];
+
+  function generateExcuse() {
+    var w = who[parseInt(Math.random() * who.length)];
+    var a = action[parseInt(Math.random() * action.length)];
+    var wh = what[parseInt(Math.random() * what.length)];
+    var wn = when[parseInt(Math.random() * when.length)];
+    
+    var sentence =  w + " " + a + " " + wh + " " + wn + ".";
+    return sentence;
+  }
+
+  var excuse = generateExcuse();
+  document.getElementById("excuse").innerHTML = "<strong>" + excuse + "</strong>";
 };
